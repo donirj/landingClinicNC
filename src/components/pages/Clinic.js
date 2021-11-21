318f4a4df3c4a431131dae7db6b769c57e61142c
andoni.resendiz <andoniresendiz@gmail.com>
2021-11-21 10:11:32 -0600

    avance clinic y doctors

--- src/components/pages/Clinic.js
+++ src/components/pages/Clinic.js
@@ -11,20 +11,20 @@ function Clinic() {
             <h3> CLINIC </h3>
         </div> 
 
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top navbar-dark bg-dark">
-        <div class="col-md-4 d-flex align-items-center">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-            <svg class="bi" width="30" height="24"></svg>
-        </a>
-        <span class="text-muted">© 2021 Company, Inc</span>
-        </div>
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top navbar-dark bg-dark">
+            <div class="col-md-4 d-flex align-items-center">
+            <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <svg class="bi" width="30" height="24"></svg>
+            </a>
+            <span class="text-muted">© 2021 Company, Inc</span>
+            </div>
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use></use></svg></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use></use></svg></a></li>
-        <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use></use></svg></a></li>
-        </ul>
-    </footer>
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use></use></svg></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use></use></svg></a></li>
+            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use></use></svg></a></li>
+            </ul>
+        </footer>
         </body>
         
     )
--- src/components/pages/Doctors.js
+++ src/components/pages/Doctors.js
@@ -6,16 +6,18 @@ function Doctors() {
             <h3 class="dr3"> MEET OUR DOCTORS </h3> 
 
             <div class="box3-1">
-            <p>Know our doctors and choose a date for your appointment</p>
-
+            <p class="txt3">Know our doctors and choose a date for your appointment</p>
+            <div><img src="/images/doctors 1.png" /></div>
             </div>
 
             <div class="box3-1">
- 
-            <p>You can take a turn and pay during your appointment</p>
+            <div class="heart"><img src="/images/heart-g09089e836_640 1.png" /></div>
+            <p class="txt3">You can take a turn and pay during your appointment</p>
             </div>
 
+            <div class="box3-2">
             <button class="btn3">See more</button>
+            </div>
 
         </body>
     )
