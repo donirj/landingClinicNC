719c98e911f930048ae2424c70bcb75ee29d8231
EnzoGRDev <enzo.g.rz.18@gmail.com>
2021-11-23 02:50:38 -0300

    avance en pagina home solo para mobile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import '../App.css'
-
+import BannerImg from '../images/banner.png'
+import {Icon} from '@iconify/react'
 function Home() {
     return (
-        <section className="operation">
-            <h3>
-                Home
-            </h3> 
-            <div className="box1 operation__card">
-                <p>
-                    SEARCH ALL YOU NEED FOR YOUR HEALTH
-                </p>
-                <button className="btn1">
+        <section className="first-section">
+            <figure className="first-section-card">
+                <h4>
+                    SEARCH ALL FOR YOUR HEALTH
+                </h4>
+                <ul className="first-section-card-list">
+                    <li><Icon icon='healthicons:doctor-outline' /></li>
+                    <li><Icon icon='healthicons:health-data-security' /></li>
+                    <li><Icon icon='medical-icon:i-laboratory' /></li>
+                    <li><Icon icon='fluent:heart-pulse-20-filled' /></li>
+                </ul>
+                <button className="button">
                     read more
                 </button>
-            </div>
+            </figure>
         </section>
     )
 }
